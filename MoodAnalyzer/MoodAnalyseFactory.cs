442ec317e7b4726205711cd56dff066afdd331d5
Sunny Jha <sunnyjha11897@gmail.com>
2020-10-14 09:54:36 +0530

    [SUNNY] Add. UseCase4 to make a object usinng reflection

---
+++ MoodAnalyzer/MoodAnalyseFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyseFactory
+    {
+        public static object CreateMoodAnalyse(string className,string constructorName)
+        {
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new MoodAnalyseException(MoodAnalyseException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyseException(MoodAnalyseException.ExceptionType.NO_SUCH_METHOD, "Constructors not found");
+            }
+        }
+    }
+}
